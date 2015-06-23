a3f9e06ab19e3be8e541ca1bec79629d21824f32
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-24 01:08:44 +0530

    Clean up : TopTaker and Example are spick and span now.

--- javascripts/widgets/TopTaker.widget.js
+++ javascripts/widgets/TopTaker.widget.js
@@ -14,40 +14,36 @@
             var self = this,
                 container = self.element,
                 options = self.options,
-                theme = options.theme,
-                iconClass = options.iconClass,
-                text = options.text,
-                imageElement = '',
-                themeString = '';
-
-            themeString = theme !== '' ? 'top-taker-' + theme : '';
-            imageElement = $(self.options.imageElement).addClass(iconClass).css('margin','0 2px 0 0');
-			//using themeString here
-			var OurEle = $('<div class="top-taker '+themeString+ '"></div>')
-                            .addClass(self.options.hiddenClass)
-                            .addClass(self.options.positionClass)
-                            .addClass(self.options.cursorClass);
-
-            $(OurEle).append(imageElement).append(text);
-
-            $(container).append(OurEle);
-
-            $(OurEle).bind('click', function () {
-                window.scrollTo(0, 0);
-            });
+                theme = options.theme !== '' ? 'top-taker-' + options.theme : '',
+                imageElement = $(options.imageElement).addClass(options.iconClass).css('margin','0 2px 0 0'),
+                hiddenClass = options.hiddenClass,
+                scrollPositionY = options.scrollPositionY,
+                theTopTaker = $('<div class="top-taker"></div>')
+                            .addClass(theme)
+                            .addClass(hiddenClass)
+                            .addClass(options.positionClass)
+                            .addClass(options.cursorClass)
+                            .append(imageElement)
+                            .append(options.text)
+                            .bind('click', function () {
+                                window.scrollTo(0, 0);
+                            });            
 
             $(window).bind('scroll', function () {
-                if (window.scrollY > self.options.scrollPositionY) {
-                    $(OurEle).removeClass(self.options.hiddenClass);
+                if (window.scrollY > scrollPositionY) {
+                    $(theTopTaker).removeClass(hiddenClass);
                 }
-                if (window.scrollY < self.options.scrollPositionY) {
-                    $(OurEle).addClass(self.options.hiddenClass);
+                if (window.scrollY < scrollPositionY) {
+                    $(theTopTaker).addClass(hiddenClass);
                 }
             });
+            
+            $(container).append(theTopTaker);
         }
         , destroy: function () {
-            $(OurEle).unbind('click');
+            $(theTopTaker).unbind('click');
             $(window).unbind('scroll');
+            $(container).html('');
         }
     });
 })(jQuery);
